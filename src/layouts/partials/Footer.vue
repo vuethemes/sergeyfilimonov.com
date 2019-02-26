f970531f7d8219418b479ddfe556179eff8cd031
Sergey <tunecatcher@yandex.ru>
2019-02-26 23:44:10 +0300

    commit

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer id="more" class="section">
+  <footer class="section">
 		<div class="container has-text-centered">
 			<p>© Сергей Филимонов 2019 ∙ <a class="my-link" href="https://github.com/sergeyfilimonov/sergeyfilimonov.com" target="_blank">Этот сайт на GitHub</a></p>
 		</div>
@@ -13,12 +13,15 @@ export default {
 </script>
 
 <style>
-  .footer-links {
-    display: flex;
-  }
-  .footer-links a {
-    font-weight: 600;
-    color: #333;
-    margin-right: 1rem;
-  }
+footer {
+  font-size: 14px;
+}
+.footer-links {
+  display: flex;
+}
+.footer-links a {
+  font-weight: 600;
+  color: #333;
+  margin-right: 1rem;
+}
 </style>
--- src/pages/partials/Intro.vue
+++ src/pages/partials/Intro.vue
@@ -4,9 +4,13 @@
       <div class="is-size-1 is-size-3-mobile has-text-centered" style="margin-bottom: 1rem;">🚀</div>
       <h1 class="has-text-black has-text-centered">
         <span class="is-size-1 is-size-3-mobile">
-          Создаю быстрые и удобные сайты
+          Создаю быстрые и удобные сайты для
           <br>
-          для <VueTyper :text="words" :type-delay="60" caret-animation="smooth" />
+          <VueTyper
+            :text="words"
+            :type-delay="60"
+            caret-animation="smooth"
+            class="is-size-1 is-size-3-mobile" />
         </span>
       </h1>
     </div>
@@ -30,6 +34,64 @@ export default {
 </script>
 
 <style lang="scss">
+@media only screen and (max-width: 568px) {
+  .vue-typer {
+    font-size: 29px;
+  .custom.char {
+    font-size: 24px;
+
+    &.typed {
+      /* Styles specific to typed characters
+         i.e. characters to the left of the caret */
+    }
+    &.selected {
+      /* Styles specific to selected characters
+         i.e. characters to the right of the caret while VueTyper's
+              'eraseStyle' is set to a selection-based style */
+    }
+    &.erased {
+      /* Styles specific to erased characters
+         i.e. characters to the right of the caret while VueTyper's
+              'eraseStyle' is set to a non-selection-based style */
+    }
+  }
+
+  .custom.caret {
+    /* Styles for the caret
+       e.g. background-color, animation, display */
+
+    &.pre-type {
+      /* Styles for the caret when it is idle before typing
+         i.e. before a string starts being typed, during 'preTypeDelay' */
+    }
+    &.pre-erase {
+      /* Styles for the caret when it is idle before erasing
+         i.e. before a string starts being erased, during 'preEraseDelay' */
+    }
+    &.idle {
+      /* Styles for the caret when it is idle, but VueTyper has not yet completed typing
+         i.e. when 'pre-type' or 'pre-erase' is set, but not 'complete' */
+    }
+    &.typing {
+      /* Styles for the caret while VueTyper is typing
+         i.e. when the caret is moving forwards */
+    }
+    &.selecting {
+      /* Styles for the caret while VueTyper is selecting
+         i.e. when the caret is moving backwards and 'eraseStyle' is
+         set to a selection-based style */
+    }
+    &.erasing {
+      /* Styles for the caret while VueTyper is erasing
+         i.e. when the caret is moving backwards and 'eraseStyle' is
+         set to a non-selection-based style */
+    }
+    &.complete {
+      /* Styles for the idle caret when VueTyper has finished all typing/erasing */
+    }
+  }
+}
+}
 @keyframes Type {
   from  {
     transform: translateX(-5px);
