3d956b36f37e4fb4e9a9d1a25a54ec881566cb97
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-14 19:12:11 +0300

    go

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="py-6">
-
+  <div class="mt-8 py-6 text-xs bg-gray-100 border-t border-gray-200">
+    <div class="container">
+      © Сергей Филимонов 2019
+    </div>
   </div>
 </template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -38,10 +38,11 @@
             </ul>
           </div>
         </div>
-        <div class="bg-gray-100 p-6">
-          <div class="">
-            <div class="font-bold text-xs text-gray-500 mb-2">
-              Из Telegram
+        <div class="">
+          <div class="bg-gray-100 p-6">
+            <div class="font-bold text-xs text-gray-500 mb-2 flex items-center">
+              <div class="pulseAnim mr-1"><div/></div>
+              Telegram
             </div>
             <div class="mb-4 pl-2 border-l border-social-telegram">
               <div class="text-sm mb-2">
@@ -53,28 +54,29 @@
             </div>
             <div class="mb-4 pl-2 border-l border-social-telegram">
               <div class="text-sm mb-2">
-                Source by Forpeople — большая библиотека дизайн-блоков для Sketch и Figma: 537 блоков, 571 элемент, 12 шаблонов страниц, всё в двух темах — светлой и тёмной.
+                colors.eva.design — генератор цветовых схем. Из особенностей — простой, приятный дизайн и, как заявлено, туда подключена нейросеть, которая врдое бы должна выдавать схемы более интересные, чем другие генераторы
               </div>
               <div class="text-xs">
-                13 июля 2019
+                12 июля 2019
               </div>
             </div>
             <div class="mb-4 pl-2 border-l border-social-telegram">
               <div class="text-sm mb-2">
-                Source by Forpeople — большая библиотека дизайн-блоков для Sketch и Figma: 537 блоков, 571 элемент, 12 шаблонов страниц, всё в двух темах — светлой и тёмной.
+                freshstatus — условно бесплатные статусные страницы для вашиъ пользователей. Статусные страницы полезны для онлайн-сервисов, набравших такую популярность, что каждое падение сервиса вызывает шквал обрашений в поддержку. Вместо этого можно дать всем возможность видеть статус работы ваших серверов и самым активным пользователям дать возможность подписаться на сбои в работе сервиса. → https://www.freshworks.com/statuspage/
               </div>
               <div class="text-xs">
-                13 июля 2019
+                11 июля 2019
               </div>
             </div>
             <div class="mb-4 pl-2 border-l border-social-telegram">
               <div class="text-sm mb-2">
-                Source by Forpeople — большая библиотека дизайн-блоков для Sketch и Figma: 537 блоков, 571 элемент, 12 шаблонов страниц, всё в двух темах — светлой и тёмной.
+                История GraphQL https://youtu.be/783ccP__No8
               </div>
               <div class="text-xs">
-                13 июля 2019
+                1 июля 2019
               </div>
             </div>
+            <a href="https://tele.click/sfilimonov" class="link">Все посты в Telegram</a>
           </div>
         </div>
       </div>
@@ -94,6 +96,7 @@ query BlogPosts {
         action
         content
         category
+        date
       }
     }
   }
