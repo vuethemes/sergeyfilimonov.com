4779c233d9c1d03b0104bed55e47e94e5db41de5
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-15 13:18:57 +0300

    go

--- src/components/WidgetTelegram.vue
+++ src/components/WidgetTelegram.vue
@@ -4,36 +4,12 @@
       <div class="pulseAnim mr-1"><div/></div>
       Telegram
     </div>
-    <div class="mb-4 pl-2 border-l border-social-telegram">
+    <div class="mb-4 pl-2 border-l border-social-telegram" v-for="post in posts">
       <div class="text-sm mb-2">
-        Source by Forpeople — большая библиотека дизайн-блоков для Sketch и Figma: 537 блоков, 571 элемент, 12 шаблонов страниц, всё в двух темах — светлой и тёмной.
+        {{ post.text }}
       </div>
       <div class="text-xs">
-        13 июля 2019
-      </div>
-    </div>
-    <div class="mb-4 pl-2 border-l border-social-telegram">
-      <div class="text-sm mb-2">
-        colors.eva.design — генератор цветовых схем. Из особенностей — простой, приятный дизайн и, как заявлено, туда подключена нейросеть, которая врдое бы должна выдавать схемы более интересные, чем другие генераторы
-      </div>
-      <div class="text-xs">
-        12 июля 2019
-      </div>
-    </div>
-    <div class="mb-4 pl-2 border-l border-social-telegram">
-      <div class="text-sm mb-2">
-        freshstatus — условно бесплатные статусные страницы для вашиъ пользователей. Статусные страницы полезны для онлайн-сервисов, набравших такую популярность, что каждое падение сервиса вызывает шквал обрашений в поддержку. Вместо этого можно дать всем возможность видеть статус работы ваших серверов и самым активным пользователям дать возможность подписаться на сбои в работе сервиса. → https://www.freshworks.com/statuspage/
-      </div>
-      <div class="text-xs">
-        11 июля 2019
-      </div>
-    </div>
-    <div class="mb-4 pl-2 border-l border-social-telegram">
-      <div class="text-sm mb-2">
-        История GraphQL https://youtu.be/783ccP__No8
-      </div>
-      <div class="text-xs">
-        1 июля 2019
+        {{ post.date }}
       </div>
     </div>
     <a href="https://tele.click/sfilimonov" class="link text-social-telegram border-social-telegram">Все посты в Telegram</a>
@@ -41,6 +17,13 @@
 </template>
 
 <script>
+import posts from '@/data/telegram.yaml'
+
 export default {
+  data() {
+    return {
+      posts: posts
+    }
+  }
 }
 </script>
