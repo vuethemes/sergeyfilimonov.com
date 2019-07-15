cf84d44d5d733a8c0a1ef45da54470b83ebc0791
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-15 09:32:10 +0300

    go

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,7 +1,7 @@
 <template>
   <g-link :to="post.path" v-if="post.slug != currentPage" class="block p-6 border-gray-200 border mb-4 hover:shadow-lg">
     <div v-html="formattedPublishDate" class="mb-2 text-xs"/>
-    <h3 class="text-2xl mb-2" v-html="post.title"/>
+    <h3 class="text-2xl mb-2 leading-none" v-html="post.title"/>
     <div v-html="post.excerpt"/>
   </g-link>
 </template>
