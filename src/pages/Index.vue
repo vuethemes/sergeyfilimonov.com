f4121d89ba785dc950d28751ad0a51d211454312
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-15 08:36:12 +0300

    go

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -69,6 +69,22 @@
               </li>
             </ul>
           </div>
+          <div class="bg-primary text-white p-6 mt-5">
+            <div class="font-bold text-xs text-white mb-2">
+              Реклама
+            </div>
+            <div class="">
+              <div class="text-sm font-bold mb-1">
+                Студия «Свой бренд»
+              </div>
+              <div class="text-sm">
+                Мы работаем как студия и агентство одновременно. Часть услуг, составляющих личный бренд, мы делаем силами внутренней команды. Например, это касается сайтов и YouTube. Часть — доверяем другим студиям, с которыми мы работали и получили хороший результат. Например, это всё, что касается SEO.
+              </div>
+              <div class="text-sm mt-2 pl-2 border-white border-l-2">
+                Заказать свой <a href="https://svoybrend.ru/svoy/site" target="_blank" class="link text-white border-white">сайт</a>, <a href="https://svoybrend.ru/svoy/youtube" target="_blank" class="link text-white border-white">YouTube-канал</a> и <a href="https://svoybrend.ru" target="_blank" class="link text-white border-white">другое</a>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="">
           <div class="bg-gray-100 p-6">
