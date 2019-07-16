cfd0c194dc5f3bf8ba5c45dbc5ae17bb8f7847d1
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-16 10:04:08 +0300

    go

--- src/components/WidgetManagement.vue
+++ src/components/WidgetManagement.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-gray-100 p-6 mt-5">
-    <div class="font-bold text-xs text-gray-500 mb-2">
+  <div class="bg-theme-backgroundGray p-6 mt-5">
+    <div class="font-bold text-xs text-theme-textSecondary mb-2">
       Технологии управления
     </div>
     <ul>
       <li class="flex items-center mb-2">
-        <div class="w-5 h-5 bg-gray-300 rounded-full mr-2"/>Продюсирование
+        <div class="w-5 h-5 bg-theme-background rounded-full mr-2"/>Продюсирование
       </li>
       <li class="flex items-center mb-2">
-        <div class="w-5 h-5 bg-gray-300 rounded-full mr-2"/>Продакт-менеджмент
+        <div class="w-5 h-5 bg-theme-background rounded-full mr-2"/>Продакт-менеджмент
       </li>
     </ul>
   </div>
--- src/components/WidgetMarketing.vue
+++ src/components/WidgetMarketing.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="bg-gray-100 p-6 mt-5">
-    <div class="font-bold text-xs text-gray-500 mb-2">
+  <div class="bg-theme-backgroundGray p-6 mt-5">
+    <div class="font-bold text-xs text-theme-textSecondary mb-2">
       Технологии онлайн-продвижения
     </div>
     <ul>
       <li class="flex items-center mb-2">
-        <div class="w-5 h-5 bg-gray-300 rounded-full mr-2"/>YouTube
+        <div class="w-5 h-5 bg-theme-background rounded-full mr-2"/>YouTube
       </li>
       <li class="flex items-center mb-2">
-        <div class="w-5 h-5 bg-gray-300 rounded-full mr-2"/>Instagram
+        <div class="w-5 h-5 bg-theme-background rounded-full mr-2"/>Instagram
       </li>
       <li class="flex items-center mb-2">
-        <div class="w-5 h-5 bg-gray-300 rounded-full mr-2"/>Email-маркетинг
+        <div class="w-5 h-5 bg-theme-background rounded-full mr-2"/>Email-маркетинг
       </li>
       <li class="flex items-center mb-2">
-        <div class="w-5 h-5 bg-gray-300 rounded-full mr-2"/>Telegram
+        <div class="w-5 h-5 bg-theme-background rounded-full mr-2"/>Telegram
       </li>
     </ul>
   </div>
