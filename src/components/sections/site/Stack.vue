aab2a6e9a46cfdd6d982e1061301aba5aab5e642
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-23 10:47:33 +0300

    с

--- src/components/sections/site/Stack.vue
+++ src/components/sections/site/Stack.vue
@@ -6,7 +6,7 @@
     <div class="grid-cols grid-cols--2m4d">
       <div class="p-4 lg:p-6 bg-theme-backgroundGray flex items-center" v-for="item in stack">
         <div class="">
-          <img :src="`/images/stack/${item.slug}.png`" :alt="item.name" class="w-12 lg:w-8 mr-4"/>
+          <img :src="`/images/stack/${item.slug}.png`" :alt="item.name" class="w-8 lg:w-12 mr-4"/>
         </div>
         <div class="text-base lg:text-xl font-bold">
           {{ item.name }}
