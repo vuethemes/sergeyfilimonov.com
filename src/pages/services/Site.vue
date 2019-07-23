a4d245d37eee31d2392dc1c7c6efb9c33e8ddeeb
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-23 11:15:46 +0300

    c

--- src/pages/services/Site.vue
+++ src/pages/services/Site.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="container section">
-      <div class="mt-4 mb-4">
+      <div class="lg:mt-4 mb-4">
         <h1 class="text-3xl lg:text-6xl mb-4 leading-none"><span class="font-bold">Разработка сайта:</span><br>хорошо, в срок, без ТЗ</h1>
         <div class="text-lg lg:text-2xl max-w-4xl">
           Сайт — точка сборки всего бренда не только для клиентов, но и для вас самих. При прочих равных бренд с хорошим сайтом всегда смотрится выигрышнее.
