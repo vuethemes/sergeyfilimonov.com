2c6e7d7dcde94d134e45e33cd0f39d385ed99b84
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-07-24 16:20:26 +0300

    c

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,6 +1,10 @@
 <template>
   <ul class="flex">
-    <li v-for="item in social" class="mr-2"><a :href="item.link" rel="nofollow noreferrer noopener" target="_blank"><i :class="`fab fa-${item.icon} text-theme-text hover:text-${item.color}`"></i></a></li>
+    <li v-for="item in social" class="mr-2">
+      <a :href="item.link" rel="nofollow noreferrer noopener" target="_blank">
+        <i :class="`${item.icon} text-theme-text hover:text-${item.color}`"></i>
+      </a>
+    </li>
   </ul>
 </template>
 
@@ -12,32 +16,38 @@ export default {
         {
           name: 'YouTube',
           link: 'https://www.youtube.com/channel/UCoj3nHKZm6r2acowfEwSFww?view_as=subscriber',
-          icon: 'youtube',
+          icon: 'fab fa-youtube',
           color: 'social-youtube'
         },
         {
           name: 'Telegram',
           link: 'https://tele.click/sfilimonov',
-          icon: 'telegram',
+          icon: 'fab fa-telegram',
           color: 'social-telegram'
         },
         {
           name: 'Instagram',
           link: 'https://instagram.com/sergeyfilimonov_com',
-          icon: 'instagram',
+          icon: 'fab fa-instagram',
           color: 'pink-500'
         },
         {
           name: 'Facebook',
           link: 'https://facebook.com/tunecatcher',
-          icon: 'facebook',
+          icon: 'fab fa-facebook',
           color: 'social-facebook'
         },
         {
           name: 'WhatsApp',
           link: 'https://api.whatsapp.com/send?phone=79261118328',
-          icon: 'whatsapp',
+          icon: 'fab fa-whatsapp',
           color: 'social-whatsapp'
+        },
+        {
+          name: 'Email',
+          link: 'mailto:email@sergeyfilimonov.com',
+          icon: 'fa fa-envelope',
+          color: 'gray-500'
         },
       ]
     }
