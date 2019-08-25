cef854a4ef80b07fa4025ddac6f8fa2389f67989
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2019-08-25 16:44:48 +0300

    c

--- src/components/WidgetTelegram.vue
+++ src/components/WidgetTelegram.vue
@@ -2,15 +2,16 @@
   <div class="bg-theme-backgroundGray p-4 lg:p-4">
     <div class="font-bold text-xs text-theme-textSecondary mb-2 flex items-center">
       <div class="pulseAnim mr-1"><div/></div>
-      Telegram
+      Telegram за неделю
     </div>
     <div class="mb-4 pl-2 border-l border-social-telegram" v-for="post in posts">
-      <div class="text-sm mb-2">
+      <div class="text-sm mb-1">
         {{ post.text }}
       </div>
-      <div class="text-xs">
+      <div v-if="post.date" class="text-xs">
         {{ post.date }}
       </div>
+      <a v-if="post.more" :href="post.more" class="text-xs link text-social-telegram border-social-telegram" target="_blank">Подробнее</a>
     </div>
     <a href="https://tele.click/sfilimonov" rel="nofollow noreferrer noopener" class="link text-social-telegram border-social-telegram" target="_blank">Все посты в Telegram</a>
   </div>
