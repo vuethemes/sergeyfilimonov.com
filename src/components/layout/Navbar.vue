aded0ebc197763b16dc957858bbe95864854c36f
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2020-01-27 19:18:05 +0300

    1

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -4,9 +4,9 @@
       <div class="flex items-center justify-between lg:flex-row">
         <div class="flex items-center">
           <div class="group">
-            <g-link to="/" class="text-base lg:text-2xl leading-tight font-mono font-bold" style="word-spacing: -6px;">Сергей Филимонов</g-link>
-            <div class="mt-1 lg:mt-0 text-xs leading-tight">
-              О&nbsp;JAMstack, Gridsome, Serverless&nbsp;и&nbsp;<g-link class="link" to="/about/">всём таком</g-link>
+            <g-link to="/" class="text-base lg:text-2xl leading-tight font-mono font-bold" style="word-spacing: -2px;">Сергей Филимонов</g-link>
+            <div class="mt-1 text-xs leading-tight">
+              О&nbsp;JAMstack<span class="hidden lg:inline">, Gridsome, Serverless</span>&nbsp;и&nbsp;<g-link class="link" to="/about/">всём таком</g-link>
             </div>
           </div>
         </div>
